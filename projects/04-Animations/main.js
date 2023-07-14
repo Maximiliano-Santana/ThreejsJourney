e5b263e2e3049512a5e1c67bfe96d01fd1be4de0
Maximiliano Santana <iimaxsany@gmail.com>
2023-07-14 10:32:51 -0600

    Made 04-Animation, learned the bases of animation libraries to animate introduction like gsap

--- projects/04-Animations/main.js
+++ projects/04-Animations/main.js
@@ -1,5 +1,6 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 const scene = new THREE.Scene();
 
@@ -99,4 +100,22 @@ const tick3 = ()=>{
   //Animation request
   window.requestAnimationFrame(tick3); 
 }
-tick3();
+//tick3();
+
+//-----------------------------------Using a Library-----------------------------------------
+//If you want to create timelines, tweens, etc, you can use GSAP. Is a library taht simplify this things.
+
+gsap.to(cube.position, {duration: 1, delay:0 , x:2}); //Method that can animates first pass the object position and then an object 
+//The animation is beeing procesed in other method thats why it doesn't depend of the framerate
+gsap.to(cube.position, {duration: 2, delay: 1, y:2});
+gsap.to(cube.position, {duration: 2, delay: 3, x:-2}); 
+gsap.to(cube.position, {duration: 1, delay: 5, y:0}); 
+gsap.to(cube.position, {duration: 1, delay: 6, x:0}); 
+
+const tick4 = () => {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick4)
+}
+tick4()
+console.log()
+
