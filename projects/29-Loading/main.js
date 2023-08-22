69a1da742605b8503f66d9afe79c58ee6d1a0e58
Maximiliano Santana <iimaxsany@gmail.com>
2023-08-22 13:53:02 -0600

    Learned how to do a loading bar and better use of loading manager with html and css

--- projects/29-Loading/main.js
+++ projects/29-Loading/main.js
@@ -4,23 +4,98 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
+import { gsap } from 'gsap'
+
+const loadingBarElement = document.querySelector('.loadingBar');
 /**
  * Loaders
  */
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onLoad = ()=>{
+    window.setTimeout(()=>{
+        gsap.to(overlayMaterial.uniforms.uAlpha, {duration: 3, value: 0})
+        loadingBarElement.style.transform = '';
+        loadingBarElement.classList.add('ended');
+    }, 500)
+}
+
+//---------------------------------------- Progress callback 
+//The progress callback returns 3 arguments
+//URL of the assets
+//How many items has been loades
+//How many items have to be loaded
+
+loadingManager.onProgress = (assetUrl, itemsLoaded, itemsTotal)=>{     
+    const progressRatio = itemsLoaded/itemsTotal;
+    loadingBarElement.style.transform = `scaleX(${progressRatio})`;
+}
+
+const gltfLoader = new GLTFLoader(loadingManager)
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
 
 /**
  * Base
  */
 // Debug
-const debugObject = {}
+const debugObject = {
+    envMapIntensity: 1,
+}
 
 // Canvas
 const canvas = document.querySelector('.experience')
 
 // Scene
 const scene = new THREE.Scene()
+
+//Overlay
+
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1);
+const overlayMaterial = new THREE.ShaderMaterial({
+    transparent: true,
+    side: THREE.DoubleSide,
+    uniforms:{
+        uAlpha: { value: 1 },
+    },
+    vertexShader: `
+        void main(){
+            gl_Position = vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        uniform float uAlpha;
+        
+        void main(){
+            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+        }
+    `
+});
+
+//Now we have the overlay we need to know when every thing is loaded
+//To know that we can use a LoadingManager as a parameter in all loaders
+
+//Now create a loading bar 
+//We are in local and its to fast to load but we cam simulate a normal Bandwidth 
+//To do it go to devtools and activate Disable cache 
+
+//Then click on the dropdown menu with the Online value 
+
+//--------------------------- Now lets create de loading bar
+//We should create it with a new plane and fill it with shaders but this time we are going to create it on html just to practice the mix bewtween html and three.js
+
+//At the end of the load the animation is jumpy, thats because two reasons
+//When we add meshes to the scene, materials, etc, those things get compiled and loaded to the GPU and it can take a few milisecons
+//The bas didn't finished its animation because there is a 0.5s transition on it
+
+//To fix this problems we just can wait a little
+
+//---------------------------- Mixing HTML and WebGL
+//Is ussually bad for performances, just because add gtml above the WebGL
+//Keep an eye on the framerate
+
+
+const overlayMesh = new THREE.Mesh(overlayGeometry, overlayMaterial);
+scene.add(overlayMesh);
 
 /**
  * Update all materials
@@ -32,6 +107,7 @@ const updateAllMaterials = () =>
         if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
         {
             // child.material.envMap = environmentMap
+            
             child.material.envMapIntensity = debugObject.envMapIntensity
             child.material.needsUpdate = true
             child.castShadow = true
@@ -57,7 +133,6 @@ environmentMap.colorSpace = THREE.SRGBColorSpace
 scene.background = environmentMap
 scene.environment = environmentMap
 
-debugObject.envMapIntensity = 2.5
 
 /**
  * Models
@@ -128,9 +203,8 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true
 })
-renderer.useLegacyLights = false
 renderer.toneMapping = THREE.ReinhardToneMapping
-renderer.toneMappingExposure = 3
+renderer.toneMappingExposure = 1
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
